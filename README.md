146a7db3d04e8e14e8315684088dc27d8e9d3dc9
YoungHyeong Ryu <wangta69@naver.com>
2017-12-15 15:38:00 +0900

    1
    1

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-# wizboard
+# Why wizboard for laravel
 
-한국형 라라벨용 게시판 입니다. 
-현재 진행중인 프로젝트에 사용하려고 간단하게 만들고 테스트 중입니다.
+한국형 라라벨용 게시판 입니다. <br />
+현재 진행중인 프로젝트에 사용하려고 간단하게 만들고 테스트 중입니다. <br />
+ <br />
+Laravel 5.4에서 테스트 되었습니다.
 
-
-## 설치방법
+## Installation
 ```
 composer require wangta69/laravel_board
 ```
 
+## Laravel 5
 
+### Setup
+Add ServiceProvider to the providers array in `config/app.php`.
+```
+Pondol/Board/BoardServiceProvider::class,
+```
---
+++ src/BoardServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+namespace Pondol\Board;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider {
+
+	/**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+		//set migrations
+        $this->publishes([
+        	__DIR__.'/migrations/' => database_path('migrations'),
+        ], 'migrations');
+
+		//set assets
+		$this->publishes([
+        	__DIR__.'/assets/' => public_path('assets/pondol/board/assets'),
+        ], 'public');
+
+		// set package route
+		if (! $this->app->routesAreCached()) {
+	        require __DIR__.'/routes.php';
+	    }
+
+		// 기본 스킨을 위한 view 경로 지정.
+		$this->loadViewsFrom(__DIR__.'/resources/views', 'pondol/board');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
