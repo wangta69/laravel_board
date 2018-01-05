30cd7acec13596fb4fe759654c2de48f97822642
YoungHyeong Ryu <wangta69@naver.com>
2018-01-05 19:43:48 +0900

    1
    1

--- src/BbsServiceProvider.php
+++ src/BbsServiceProvider.php
@@ -4,7 +4,7 @@ namespace Pondol\Bbs;
 //use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 //use Illuminate\Support\Facades\App;
-//use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Log;
 //use Route;
 
 class BbsServiceProvider extends ServiceProvider {
@@ -44,11 +44,19 @@ class BbsServiceProvider extends ServiceProvider {
         }
         
 
-		//set migrations
+		//set migrations (automatic)
+		/*
+		Log::info(__DIR__.'/migrations/');
+        Log::info(realpath(__DIR__.'/migrations/'));
+		$this->loadMigrationsFrom(__DIR__.'/migrations/');
+        $this->artisan('migrate');
+        */
+    
+        //if you use this one you have to command php artisan migrate
         $this->publishes([
         	__DIR__.'/migrations/' => database_path('migrations'),
         ], 'migrations');
-        
+     
 
 		//set assets
 		$this->publishes([
@@ -60,14 +68,14 @@ class BbsServiceProvider extends ServiceProvider {
         // - first the published views (in case they have any changes)
          $this->publishes([
             __DIR__.'/resources/views/bbs' => resource_path('views/bbs'),
-        ], 'public');
+        ]);
         // - loadViews  : 상기와 다른 점음  resources/views/bbs 에 없을 경우 아래 것에서 처리한다. for user modify
          $this->loadViewsFrom(__DIR__.'/resources/views/bbs', 'bbs');
         
         //default controller file set
         $this->publishes([
             __DIR__.'/Https/Controllers/Bbs/' => app_path('Http/Controllers/Bbs'),
-        ], 'public');
+        ]);
 
     }
 }
--- src/migrations/2018_01_05_000001_create_pondol_bbs_tables.php
+++ src/migrations/2018_01_05_000001_create_pondol_bbs_tables.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
 
-class CreateWizboardTables extends Migration
+class CreatePondolBbsTables extends Migration
 {
     /**
      * Run the migrations.
@@ -12,44 +12,44 @@ class CreateWizboardTables extends Migration
      */
     public function up()
     {
-    	// 게시판 설정값 담을 테이블
-		Schema::create('bbs_tables', function(BluePrint $table) {
-			$table->engine = 'InnoDB';
-			
-			$table->increments('id');
-			$table->string('name');
-			$table->string('table_name');
-			$table->string('skin');
-			$table->timestamps();
-			$table->softDeletes();
-		});
-		
-		// 게시글 담을 테이블
-		// 새로운 게시판을 생성할시 이 아래 정의한 테이블들만 새로 추가하면 됩니다.
-        Schema::create('bbs_articles', function(BluePrint $table) {
-        	$table->increments('id');
-			
-			$table->integer('user_id')->unsigned();
-			$table->integer('cate_id')->unsigned();
-			$table->string('title', '255');
-			$table->text('content');
-			$table->integer('hit')->unsigned();
-			$table->timestamps();
-			$table->softDeletes();
+        // 게시판 설정값 담을 테이블
+        Schema::create('bbs_tables', function(BluePrint $table) {
+            $table->engine = 'InnoDB';
+            
+            $table->increments('id');
+            $table->string('name');
+            $table->string('table_name');
+            $table->string('skin');
+            $table->timestamps();
+            $table->softDeletes();
         });
-		
-		// 게시글에 연결된 파일 담을 테이블
-		Schema::create('bbs_files', function(BluePrint $table) {
-			$table->engine = 'InnoDB';
-			
-			$table->increments('id');
-			$table->integer('bbs_articles_id')->unsigned();
-			$table->string('filename');
-			$table->integer('rank')->unsigned();
-			$table->timestamps();
-			
-			$table->foreign('bbs_articles_id')->references('id')->on('bbs_articles');
-		});
+        
+        // 게시글 담을 테이블
+        // 새로운 게시판을 생성할시 이 아래 정의한 테이블들만 새로 추가하면 됩니다.
+        Schema::create('bbs_articles', function(BluePrint $table) {
+            $table->increments('id');
+            
+            $table->integer('user_id')->unsigned();
+            $table->integer('cate_id')->unsigned();
+            $table->string('title', '255');
+            $table->text('content');
+            $table->integer('hit')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        
+        // 게시글에 연결된 파일 담을 테이블
+        Schema::create('bbs_files', function(BluePrint $table) {
+            $table->engine = 'InnoDB';
+            
+            $table->increments('id');
+            $table->integer('bbs_articles_id')->unsigned();
+            $table->string('filename');
+            $table->integer('rank')->unsigned();
+            $table->timestamps();
+            
+            $table->foreign('bbs_articles_id')->references('id')->on('bbs_articles');
+        });
     }
 
     /**
@@ -59,12 +59,12 @@ class CreateWizboardTables extends Migration
      */
     public function down()
     {
-		Schema::table('articles_files', function($table) {
-			$table->dropForeign('bbs_files_bbs_articles_id_foreign');
-		});
-		
-    	Schema::drop('bbs_tables');
-		Schema::drop('bbs_articles');
-		Schema::drop('bbs_files');
+        Schema::table('articles_files', function($table) {
+            $table->dropForeign('bbs_files_bbs_articles_id_foreign');
+        });
+        
+        Schema::drop('bbs_tables');
+        Schema::drop('bbs_articles');
+        Schema::drop('bbs_files');
     }
 }
