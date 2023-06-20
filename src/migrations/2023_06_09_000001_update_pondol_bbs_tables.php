07b072946027332710c8934f9f228d7c71552056
younghyeong ryu <wangta69@naver.com>
2023-06-20 15:54:38 +0900

    t
    t

--- src/migrations/2023_06_09_000001_update_pondol_bbs_tables.php
+++ src/migrations/2023_06_09_000001_update_pondol_bbs_tables.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
 
-class UpdatePondolBbsArticles extends Migration
+class UpdatePondolBbsArticles01 extends Migration
 {
     /**
      * Run the migrations.
--- src/migrations/2023_06_12_000001_create_pondol_bbs_config.php
+++ src/migrations/2023_06_12_000001_create_pondol_bbs_config.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
 
-class CreatePondolBbsTables extends Migration
+class UpdatePondolBbsTables01 extends Migration
 {
     /**
      * Run the migrations.
