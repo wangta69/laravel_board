e0592b15f2da0d3a403479fc50ff572577ef4b36
younghyeong ryu <wangta69@naver.com>
2024-07-23 14:01:17 +0900

    t

--- src/migrations/2024_07_18_000001_create_item_comments.php
+++ src/migrations/2024_07_18_000001_create_item_comments.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
 
-class CreateBbsItemCommentsTables extends Migration
+class CreateItemComments extends Migration
 {
   /**
    * Run the migrations.
